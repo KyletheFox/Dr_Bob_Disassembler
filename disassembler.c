acb0bc526b30ed9b316c64a585966febedd99251
KyletheFox <ko163@me.com>
2015-11-20 00:20:19 -0500

    SIM 3 - Working up to 8000 instructions

--- disassembler.c
+++ disassembler.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include "disassembler.h"
 
-void getInstruction(int acc, char *);
+void getInstruction(int, char *);
 
 int main(int argc, char *argv[]) {
 	unsigned int ip;
@@ -17,29 +17,24 @@ int main(int argc, char *argv[]) {
       		exit(1);
     	}
 	
-
 	// Malloc tempreset
-	if ((temp = (char*)malloc(1000)) == NULL)	{
+	if ((temp = (char*)malloc(10*sizeof(char))) == NULL)	{
 		printf("ERROR\n");
 		exit(2);
 	}
 
-	printf("fp: %x\n", fp);
-
 	do {
-		fscanf(fp, "%d %d", ip, acc);
-		printf("ip: %d\n", ip);
-		printf("acc: %d\n", acc);
+		fscanf(fp, "%d %d", &ip, &acc);
+		printf("%3d %d ", ip, acc);
 		getInstruction(acc, temp);
-	} while (feof(fp));
+	} while (!feof(fp));
 
 	fclose(fp);
 
 	return 0;
 }
 
-void getInstruction(int acc, char *opCode) {
-	char *tempStr;
+void getInstruction(int acc, char *tempStr) {
 	char temp;
 
 	temp = acc/1000;
@@ -47,33 +42,30 @@ void getInstruction(int acc, char *opCode) {
 	switch(temp) {
 		case HALT:
 			temp = acc%1000;
-			if(temp == 0 && *(tempStr-1) != "HALT" && *(tempStr-1) != ".word")
-				*tempStr = "HALT";
-			else
-				*tempStr = ".word";
+			tempStr = "HALT";
 		case LD:
 			temp = acc%1000;
-			*tempStr = strcat("LD ", temp);
+			sprintf(tempStr, "LD %d", temp);
 			break;
 		case ST:
 			temp = acc%1000;
-			*tempStr = strcat("ST ", temp);
+			sprintf(tempStr, "ST %d", temp);
 			break;
 		case ADD:
 			temp = acc%1000;
-			*tempStr = strcat("ADD ", temp);
+			sprintf(tempStr, "ADD %d", temp);
 			break;
 		case SUB:
 			temp = acc%1000;
-			*tempStr = strcat("SUB ", temp);
+			sprintf(tempStr, "SUB %d", temp);
 			break;
 		case LDA:
 			temp = acc%1000;
-			*tempStr = strcat("LDA ", temp);
+			sprintf(tempStr, "LDA %d", temp);
 			break;
 		case JMP:
 			temp = acc%1000;
-			*tempStr = strcat("JMP ", temp);
+			sprintf(tempStr, "JMP %d", temp);
 			break;
 		case SKIPSET:
 			
@@ -82,33 +74,33 @@ void getInstruction(int acc, char *opCode) {
 			
 			switch(temp) {
 				case SKIP:
-					temp%10;
-					*tempStr = strcat("SKIP %r", temp);
+					temp%=10;
+					sprintf(tempStr, "SKIP r", temp);
 					break;
 
 				case SKEQ:
-					temp%10;
-					*tempStr = strcat("SKEQ %r", temp);
+					temp%=10;
+					sprintf(tempStr, "SKEQ r", temp);
 					break;
 				case SKNE:
-					temp%10;
-					*tempStr = strcat("SKNE %r", temp);
+					temp%=10;
+					sprintf(tempStr, "SKNE r", temp);
 					break;
 				case SKGT:
-					temp%10;
-					*tempStr = strcat("SKGT %r", temp);
+					temp%=10;
+					sprintf(tempStr, "SKGT r", temp);
 					break;
 				case SKGE:
-					temp%10;
-					*tempStr = strcat("SKGE %r", temp);
+					temp%=10;
+					sprintf(tempStr, "SKGE r", temp);
 					break;
 				case SKLT:
-					temp%10;
-					*tempStr = strcat("SKLT %r", temp);
+					temp%=10;
+					sprintf(tempStr, "SKLT r", temp);
 					break;
 				case SKLE:
-					temp%10;
-					*tempStr = strcat("SKLE %r", temp);
+					temp%=10;
+					sprintf(tempStr, "SKLE r", temp);
 					break;		
 			}
 
@@ -120,7 +112,7 @@ void getInstruction(int acc, char *opCode) {
 			
 			break;		
 		}
-			printf("opCode: %s\n", tempStr);		
+			printf("%s\n", tempStr);		
 
 	
 
